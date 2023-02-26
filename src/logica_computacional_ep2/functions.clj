4926832c923fcd5f2989427497b3279109f41b79
fofinhoinsider <andrei.sgi97@gmail.com>
2023-02-26 17:36:47 -0300

    initial generate language chains

--- src/logica_computacional_ep2/functions.clj
+++ src/logica_computacional_ep2/functions.clj
@@ -1,44 +1,37 @@
 (ns logica-computacional-ep2.functions
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]
-			[clojure.string :as str]))
+            [clojure.string :as str]))
 
 (defn extract-data
-	[fname]
-	(let [data (slurp fname)
-		data-splitted (str/split data #"\n")
-	]
-	data-splitted
-	)
-)
+  [fname]
+  (let [data (slurp fname)
+        data-splitted (str/split data #"\n")]
+    data-splitted))
 
 (defn get-non-terminal-symbols
-	[data-splitted]
-	(str/split (first data-splitted) #" ")
-)
+  [data-splitted]
+  (str/split (first data-splitted) #" "))
 
 (defn get-terminal-symbols
-	[data-splitted]
-	(str/split (second data-splitted) #" ")
-)
+  [data-splitted]
+  (str/split (second data-splitted) #" "))
 
 (defn get-inital-symbol
-	[data-splitted]
-	(last data-splitted)
-)
+  [data-splitted]
+  (last data-splitted))
 
 (defn build-language-hash-map
-	[data-splitted non-terminal-symbols]
-	;; (hash-map)
-	(let [rule-hash-map {}]
-	(for [i (range (count non-terminal-symbols))]
-			;; (assoc rule-hash-map (nth non-terminal-symbols i)  (str/split (nth data-splitted (+ i 2)) #" "))
-			
-	)
-	(println rule-hash-map)
-	)
-)
+  [data-splitted non-terminal-symbols]
+  (reduce (fn [hash [index key]] (assoc hash key  (str/split (nth data-splitted (+ index 2)) #" "))) {} (map-indexed (fn [idx itm] [idx itm]) non-terminal-symbols)))
 
-;; (defn generate-language-chain
-;; 	[chain]
-;; )
+(defn has-only-terminal-symbols
+  [word terminal-symbols]
+  (let [pattern (re-pattern (str "[" (reduce str terminal-symbols) "]*"))]
+    (re-matches pattern word)))
+
+(defn generate-language-chains
+  [index rule-hash-map word-length terminal-symbols language-chains]
+  (if (= index (count language-chains))
+    language-chains
+    nil))
