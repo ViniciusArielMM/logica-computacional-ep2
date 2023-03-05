b27a1b4859e44c5fea3db97dabca6e198d84d74b
ViniciusArielMM <vinicius@madmimic.com>
2023-03-05 17:27:06 -0300

    Finished implementation

--- src/logica_computacional_ep2/core.clj
+++ src/logica_computacional_ep2/core.clj
@@ -1,11 +1,29 @@
 (ns logica-computacional-ep2.core
   (:gen-class)
   (:require
-    [logica-computacional-ep2.functions :refer [extract-data] :as functions]
+    ;; [logica-computacional-ep2.functions :refer [extract-data] :as functions]
+    [logica-computacional-ep2.functions :refer :all]
   )
 )
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+
+  (def data-splitted (extract-data (nth args 0)))
+  (def terminal-symbols (get-terminal-symbols data-splitted ))
+  (def non-terminal-symbols (get-non-terminal-symbols data-splitted))
+  (def initial-symbol (get-inital-symbol data-splitted ))
+  (def rule-hash-map (build-language-hash-map data-splitted non-terminal-symbols ))
+  (def language-chains (generate-language-chains 0 rule-hash-map (count (nth args 1)) terminal-symbols non-terminal-symbols [initial-symbol]))
+
+  (print "Cadeias de tamanho maximo ")
+  (print (count (nth args 1)))
+  (print ": ")
+  (println language-chains)
+
+  (println (type (first language-chains)))
+  (println (type (nth args 1)))
+
+  (if (some #(= % (nth args 1)) language-chains)
+    (println "Cadeia foi aceita")
+    (println "Cadeia nao foi aceita")))
--- src/logica_computacional_ep2/functions.clj
+++ src/logica_computacional_ep2/functions.clj
@@ -30,12 +30,39 @@
   (let [pattern (re-pattern (str "[" (reduce str terminal-symbols) "]*"))]
     (re-matches pattern word)))
 
+(defn find-first-non-terminal-symbol-index
+  [language-chain non-terminal-symbols]
+  (loop [index 0]
+    (if (>= index (count language-chain))
+      nil
+      (if (some #{(subs language-chain index (inc index))} non-terminal-symbols)
+        index
+        (recur (inc index))))))
+
+(defn replace-at-index
+  [index-to-replace language-chain formations]
+  (vec (for [formation formations]
+         (apply str (concat (take index-to-replace language-chain) formation (drop (inc index-to-replace) language-chain))))))
+
+(defn replace-language-chain-with-formations
+  [language-chains index formations]
+  (concat (subvec language-chains 0 index) formations (subvec language-chains (inc index))))
+
+
+(defn remove-long-chains [language-chains max-length]
+  (vec (filter #(<= (count %) max-length) language-chains)))
+
 (defn generate-language-chains
-  [index rule-hash-map word-length terminal-symbols language-chains]
-  (if (= index (count language-chains))
+  [index rule-hash-map word-length terminal-symbols non-terminal-symbols language-chains]
+  (if (or (= index (count language-chains)) (> index (count language-chains)))
     language-chains
     (if (has-only-terminal-symbols (nth language-chains index) terminal-symbols)
-		(generate-language-chains (+ index 1) rule-hash-map word-length terminal-symbols language-chains)
-		()		;;continuar lógica
-	)
-	))
+		(generate-language-chains (+ index 1) rule-hash-map word-length terminal-symbols non-terminal-symbols language-chains)
+		
+    (let [first-non-terminal-symbol-index (find-first-non-terminal-symbol-index (nth language-chains index) non-terminal-symbols)]
+      (if first-non-terminal-symbol-index
+        (let [possible-formations (remove-long-chains (replace-at-index first-non-terminal-symbol-index (nth language-chains index) (get rule-hash-map (str (nth (nth language-chains index) first-non-terminal-symbol-index)))) word-length)]
+        (let [new-language-chains (vec (replace-language-chain-with-formations language-chains index possible-formations))]
+          (generate-language-chains index rule-hash-map word-length terminal-symbols non-terminal-symbols new-language-chains)))
+        (generate-language-chains (+ index 1) rule-hash-map word-length terminal-symbols non-terminal-symbols language-chains)
+      )))))
